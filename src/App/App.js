f0ae2ad101c6eb7669337a5af7ca6ed1ebbe0adf
NicolasLoppin66 <loppin.n66@gmail.com>
2023-02-23 16:43:42 +0100

    Montage avec un fonctionnement similaire du Singleton

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+class App {
+
+    start() {
+        console.log('App démarrer ...');
+    }
+}
+
+const app = new App();
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello world');
+import app from "./App/App";
+
+app.start();
