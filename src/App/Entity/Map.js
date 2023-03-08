786d0576cc7190b514441b802c4f5ab8881081d1
NicolasLoppin66 <loppin.n66@gmail.com>
2023-03-08 14:13:11 +0100

    Mise en place de la carte avec utilisation de la clé d'API

--- src/App/Entity/Map.js
+++ src/App/Entity/Map.js
@@ -1,6 +1,7 @@
 import mapboxgl from "mapbox-gl";
 
 import Config from "../../../app.config.json"
+import { ClearStorageControl } from "../Control/ClearStorageControl";
 
 export class Map {
     map;
@@ -14,12 +15,23 @@ export class Map {
     }
 
     mapInit() {
+        // Création de la div dans le document (dans la page du site)
+        this.elDivMap = document.createElement('div');
+        this.elDivMap.id = 'map';
+
+        document.body.append(this.elDivMap);
+
+        // Instancie la carte et récupere la clé api.
+        mapboxgl.accessToken = Config.api.mapbox_gl.api_key;
+
         this.map = new mapboxgl.Map({
-            style: Config.api.mapbox_gl.map_style.streets, // Vas fouiller dans le ficher Config dans un systeme d'indentation.
-            center: { lng: 2.2137, lat: 46.2276 }, // Centre la carte au niveau de la France.
-            zoom: 5, // Mais un zoom initiale de 5.
-            maxZoom: 12, // Le zoom est restraint a 12 au maximum. (0 - 24)
-            minZoom: 3 // Le zoom est restraint a 3 au minimum. (0 - 24)
+            attributionControl: false,
+            container: this.elDivMap, // Récupération de l'identifiant du container de la map (ID : #map)
+            style: Config.api.mapbox_gl.map_style.streets, // Vas récupéré dans le config le style de la carte MapBox
+            center: { lng: 6.0935318, lat: 46.5975118 }, // Centre la carte au niveau de la France. 46.8789065,6.1736436,6
+            zoom: 5.5, // Mais un zoom initiale de 5.
+            maxZoom: 12, // Le zoom est restraint a 12 au maximum. (0 - 24) | (0 < x < 24)
+            minZoom: 3 // Le zoom est restraint a 3 au minimum. (0 - 24) | (0 < x < 24)
         });
     }
 
@@ -29,7 +41,7 @@ export class Map {
             showCompass: true, // Permet l'apparition du compas sur la carte mapbox
             showZoom: true, // 
         });
-        this.map.addControl(NavigationControl, 'bottom-right')
+        this.map.addControl(NavigationControl, 'top-left')
     }
 
     geolocationControl() {
@@ -43,10 +55,16 @@ export class Map {
 
     getCoordinate() {
         this.map.on('click', (evt) => {
-            this.evtLat.value = evt.lnhLat.lat
+            console.log(evt);
+            this.evtLat.value = evt.lngLat.lat
+            console.log(evt.lngLat.lat);
             this.evtLng.value = evt.lngLat.lng
-        })
+            console.log(evt.lngLat.lng);
+        });
     }
 
-
+    clearStorageControl() {
+        const clearStorageControl = new ClearStorageControl();
+        this.map.addControl(clearStorageControl, 'top-left');
+    }
 }
